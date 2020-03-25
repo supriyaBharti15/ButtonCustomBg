c472865377cab310b2b840f12132f796e8334a0e
supriyaBharti15 <supriya.simi@gmail.com>
2020-03-25 20:48:14 +0530

    added sort way to change bg color dynamically using array

--- app/src/main/java/com/example/buttoncustombg/MainActivity.kt
+++ app/src/main/java/com/example/buttoncustombg/MainActivity.kt
@@ -3,14 +3,34 @@ package com.example.buttoncustombg
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    /*
+    de-clear all the drawable in int array..
+     */
+    val bgDrawableIds = intArrayOf(
+        R.drawable.btn_center_gradient,
+        R.drawable.btn_edge_color,
+        R.drawable.btn_gradient,
+        R.drawable.btn_solid_color
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var btnBackground = findViewById(R.id.buttonBg) as Button
         var bgColor: Int = 1
+
+        var intCol: Int = 0
+        btnBackground.setOnClickListener {
+            btnBackground.background = getDrawable(bgDrawableIds[intCol % bgDrawableIds.size])
+            intCol++;
+        }
+
+        /*
+        2nd Sollution in a long way
+         */
         btnBackground.setOnClickListener { v ->
             if (bgColor == 1) {
                 btnBackground.background = resources.getDrawable(R.drawable.btn_center_gradient)
@@ -20,11 +40,12 @@ class MainActivity : AppCompatActivity() {
                 bgColor = 3
             } else if (bgColor == 3) {
                 btnBackground.background = resources.getDrawable(R.drawable.btn_gradient)
-                bgColor=4
-            }else if(bgColor==4){
-                btnBackground.background=resources.getDrawable(R.drawable.btn_solid_color)
-                bgColor=1
+                bgColor = 4
+            } else if (bgColor == 4) {
+                btnBackground.background = resources.getDrawable(R.drawable.btn_solid_color)
+                bgColor = 1
             }
         }
+
     }
 }
